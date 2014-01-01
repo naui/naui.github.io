44e5414bf7f0fd65ee1c768b013db5bc7f5f65ca
Daniel Nauerz <d.nauerz@gmail.com>
2014-01-01 15:19:02 +0000

    ,

--- src/com/test/watchclip/ClipWatcherReceiver.java
+++ src/com/test/watchclip/ClipWatcherReceiver.java
@@ -4,17 +4,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-/**
- * Simple receiver that will handle the boot completed intent and send the intent to 
- * launch the BootDemoService.
- * @author BMB
- *
- */
-public class ClipWatcherReceiver extends BroadcastReceiver {
- @Override
- public void onReceive(final Context context, final Intent bootintent) {
-  Intent mServiceIntent = new Intent();
-mServiceIntent.setAction("com.test.watchclip.PrimaryService");
-  context.startService(mServiceIntent);
- }
+public class ClipWatcherReceiver extends BroadcastReceiver
+{
+    @Override
+    public void onReceive(Context context, Intent intent) 
+    {
+            Toast.makeText(context, "Booooooot", Toast.LENGTH_SHORT).show();
+
+    }
+}
 }
--- src/com/test/watchclip/PrimaryService.java
+++ src/com/test/watchclip/PrimaryService.java
@@ -3,7 +3,8 @@ package com.test.watchclip;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-
+import android.os.*;
+import android.app.*;
 import android.util.Log;
 import android.widget.Toast;
 import java.util.Timer;
@@ -11,9 +12,10 @@ import java.util.TimerTask;
 
 public class PrimaryService extends Service
 {
-    private static Timer timer = new Timer(); 
-    private Context ctx;
-    
+   private static Timer timer = new Timer();
+
+   private Context ctx;
+
    public IBinder onBind(Intent arg0)
    {
       return null;
@@ -23,15 +25,16 @@ public class PrimaryService extends Service
    public void onCreate()
    {
       super.onCreate();
-          ctx = this; 
-          startService();
+      ctx = this;
+      startService();
    }
 
-    private void startService()
-    {           
-        timer.scheduleAtFixedRate(new mainTask(), 0, 5000);
-    }
-       @Override
+   private void startService()
+   {
+      timer.scheduleAtFixedRate(new mainTask(), 0, 5000);
+   }
+
+   @Override
    public void onStart(Intent intent, int startId)
    {
       Toast.makeText(this, "My Service Started", Toast.LENGTH_LONG).show();
@@ -43,20 +46,20 @@ public class PrimaryService extends Service
       Toast.makeText(this, "Service Stopped ...", Toast.LENGTH_SHORT).show();
    }
 
-    private class mainTask extends TimerTask
-    { 
-        public void run() 
-        {
-            toastHandler.sendEmptyMessage(0);
-        }
-    }    
-        private final Handler toastHandler = new Handler()
-    {
-        @Override
-        public void handleMessage(Message msg)
-        {
-            Toast.makeText(getApplicationContext(), "test", Toast.LENGTH_SHORT).show();
-        }
-       };
-       }   
-};
+   private class mainTask extends TimerTask
+   {
+      public void run()
+      {
+         toastHandler.sendEmptyMessage(0);
+      }
+   }
+
+   private final Handler toastHandler = new Handler()
+   {
+      @Override
+      public void handleMessage(Message msg)
+      {
+         Toast.makeText(getApplicationContext(), "test", Toast.LENGTH_SHORT).show();
+      }
+   };
+}
