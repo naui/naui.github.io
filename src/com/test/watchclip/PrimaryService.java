419d1942b4f109f53e29d1338f561b7d2c9ee5cf
Daniel Nauerz <d.nauerz@gmail.com>
2014-01-01 11:44:47 +0000

    x

--- src/com/test/watchclip/PrimaryService.java
+++ src/com/test/watchclip/PrimaryService.java
@@ -5,34 +5,52 @@ import android.os.*;
 import android.app.*;
 import android.util.Log;
 import android.widget.Toast;
- 
-public class PrimaryService extends Service{
-    public IBinder onBind(Intent arg0) {
-        return null;
-    }
- 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        Toast.makeText(this, "Congrats! MyService Created", Toast.LENGTH_LONG).show();
-        MyTimerTask myTask = new MyTimerTask();
-        Timer myTimer = new Timer();
-        myTimer.schedule(myTask, 3000, 1500);  
-    }
- class MyTimerTask extends TimerTask {
-	  public void run() {
-Toast.makeText(this, "My Service timer", Toast.LENGTH_LONG).show();
-	  }
-	}
-    @Override
-    public void onStart(Intent intent, int startId) {
-        Toast.makeText(this, "My Service Started", Toast.LENGTH_LONG).show();
-        Log.d(TAG, "onStart");    
-    }
- 
-    @Override
-    public void onDestroy() {
-        Toast.makeText(this, "MyService Stopped", Toast.LENGTH_LONG).show();
-        Log.d(TAG, "onDestroy");
-    }
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class PrimaryService extends Service
+{
+   public IBinder onBind(Intent arg0)
+   {
+      return null;
+   }
+
+   @Override
+   public void onCreate()
+   {
+      super.onCreate();
+      Toast.makeText(this, "Congrats! MyService Created", Toast.LENGTH_LONG).show();
+      MyTimerTask myTask = new MyTimerTask();
+      Timer myTimer = new Timer();
+      myTimer.schedule(myTask, 3000, 1500);
+   }
+
+   class MyTimerTask extends TimerTask
+   {
+      public void run()
+      {
+          Activity_name.this.runOnUiThread(new Runnable() {
+      @Override
+      public void run() {
+        Toast.makeText(Activity_name.this,
+                  getResources().getString(R.string.st_toast_msg_stopped),
+                  Toast.LENGTH_LONG).show();
+      }
+    });
+      }
+   }
+   
+
+
+   @Override
+   public void onStart(Intent intent, int startId)
+   {
+      Toast.makeText(this, "My Service Started", Toast.LENGTH_LONG).show();
+   }
+
+   @Override
+   public void onDestroy()
+   {
+      Toast.makeText(this, "MyService Stopped", Toast.LENGTH_LONG).show();
+   }
 }
